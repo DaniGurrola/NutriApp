34fadd0a56cfd74f760dacaf8aeb1fa313d2c708
DaniGurrola <23308060610671@cetis61.edu.mx>
2025-11-06 10:26:18 -0500

    para mandar

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/iniciosesion')
-def iniciosesion():
-    return render_template('iniciosesion.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/recetas')
 def recetas():
@@ -16,24 +16,11 @@ def recetas():
 
 @app.route('/analisis', methods=['GET', 'POST'])
 def analisis():
-    resultado = None
-
-    if request.method == 'POST':
-        comida = request.form['comida']
-        calorias = int(request.form['calorias'])
-        grasas = int(request.form['grasas'])
-        azucar = int(request.form['azucar'])
-
-        if calorias < 300 and grasas < 10 and azucar < 5:
-            resultado = f"La comida '{comida}' es saludable."
-        else:
-            resultado = f"La comida '{comida}' no es saludable."
-
     return render_template('analisis.html', resultado=resultado)
 
-@app.route('/planes')
-def planes():
-    return render_template('planes.html')
+@app.route('/calculadora')
+def calculadora():
+    return render_template('calculadora.html')
 
 @app.route('/registro')
 def registro():
